f8ec502c9c96d0a404b1d2789a0d07e91c304762
Cathy Zhou <cathyzbn@stanford.edu>
2022-04-08 14:16:55 -0700

    final

--- test_client/client.ts
+++ test_client/client.ts
@@ -6,14 +6,41 @@ const host = 'localhost';
 
 // Create a new TCP client.
 const client = new Net.Socket();
+console.log("created new socket")
 // Send a connection request to the server.
 client.connect({ port: port, host: host }, function() {
     // If there is no error, the server has accepted the request and created a new 
     // socket dedicated to us.
-    console.log('TCP connection established with the server.');
+    // console.log('TCP connection established with the server.');
 
     // The client can now send data to the server by writing to its socket.
-    client.write(JSON.stringify({"type": "hell", "version": "0.8.0", "agent": "Old Peking"}) + "\n");
+    
+    // testing message before hello
+    // client.write(JSON.stringify({"type": "getpeers"}) + "\n");
+    
+    // testing hello
+    client.write(JSON.stringify({"type": "hello", "version": "0.8.0", "agent": "Old Peking"}) + "\n");
+    
+    // testing getpeers
+    // client.write(JSON.stringify({"type": "getpeers"}) + "\n");
+    
+    // testing separated message
+    client.write("{\"type\": \"ge");
+    for (let i = 0; i < 1000000; i++) {
+    }
+    client.write("tpeers\"}" + "\n");
+
+    // // testing errors
+    // console.log("1");
+    // client.write("Wbgygvf7rgtyv7tfbgy{{{" + "\n");
+    // console.log("2");
+    // client.write(JSON.stringify({"type":"diufygeuybhv"}) + "\n");
+    // console.log("3");
+    // client.write(JSON.stringify({"type":"hello"}) + "\n");gi
+    // console.log("4");
+    // client.write(JSON.stringify({"type":"hello","version":"jd3.x"}) + "\n");
+    // console.log("5");
+    // client.write(JSON.stringify({"type":"hello","version":"5.8.2"}) + "\n");
 });
 
 // The client can also receive data from the server by reading from its socket.
