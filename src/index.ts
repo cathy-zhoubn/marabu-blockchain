6973734bf1469001d1e837ab37e26a937fe3f4ca
Cathy Zhou <cathyzbn@stanford.edu>
2022-05-29 01:02:23 -0700

    6

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import {run_server} from './server';
 import {run_client} from './client';
 import { expose } from "threads/worker"
-import { mine } from './mine_block';
   
 
 expose(function server (){
