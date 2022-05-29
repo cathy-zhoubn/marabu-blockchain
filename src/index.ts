e7a8748e35dd68d9d6a6bace1aefdbf3997dfceb
Cathy Zhou <cathyzbn@stanford.edu>
2022-05-29 00:45:20 -0700

    6

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,14 @@
 import {run_server} from './server';
 import {run_client} from './client';
-import { spawn, Thread, Worker } from "threads"
 import { expose } from "threads/worker"
+import { mine } from './mine_block';
   
 
-expose(function server (){
-    run_server();
-    run_client();
-})
+// expose(function server (){
+//     run_server();
+//     run_client();
+// })
 
-async function main() {
-    const mine = await spawn(new Worker("./workers/mine"));
-    const server = await spawn(new Worker("./workers/server"))
-    await mine();
-    await server();
-  
-    await Thread.terminate(mine);
-    await Thread.terminate(server);
-  }
-  
-  main().catch(console.error)
+run_server();
+run_client();
+mine();
